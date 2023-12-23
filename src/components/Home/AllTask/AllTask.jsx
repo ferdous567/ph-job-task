bb5bc373f73f452397f2de0b13667582ea9d4d1c
ferdous <papiaferdous43@gmail.com>
2023-12-24 00:24:51 +0600

    some changes

--- src/components/Home/AllTask/AllTask.jsx
+++ src/components/Home/AllTask/AllTask.jsx
@@ -21,7 +21,7 @@ const AllTask = () => {
     return (
         <div className="text-center">
             <h3 className="text-4xl font-bold text-sky-600 underline text-center">All Task Item</h3>
-            <div className="grid grid-cols-4 gap-6 my-6 w-2/3 mx-auto">
+            <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 my-6 md:w-2/3 mx-auto">
                 {
                     tasks?.map(task => <div data-aos="flip-left"
                         data-aos-easing="ease-out-cubic"
